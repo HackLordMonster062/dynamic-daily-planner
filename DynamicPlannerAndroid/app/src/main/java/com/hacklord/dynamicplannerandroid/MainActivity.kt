085b801af2c76e87fd82f1a7447a986472b8f463
HackLordMonster062 <hackmonster062@gmail.com>
2024-10-30 19:50:07 +0200

    android init

---
+++ DynamicPlannerAndroid/app/src/main/java/com/hacklord/dynamicplannerandroid/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hacklord.dynamicplannerandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hacklord.dynamicplannerandroid.ui.theme.DynamicPlannerAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DynamicPlannerAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DynamicPlannerAndroidTheme {
+        Greeting("Android")
+    }
+}
